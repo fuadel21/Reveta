aba3c1799b946a3e32775d72ef25653c3e6c707b
fuadel21 <159694553+fuadel21@users.noreply.github.com>
2026-07-03 08:55:07 +0200

    Añade noindex a pagina 404

--- src/App.tsx
+++ src/App.tsx
@@ -78,7 +78,7 @@ const App = () => (
                 <Route path="/terms" element={<Terms />} />
                 <Route path="/privacy" element={<Privacy />} />
                 <Route path="/cookies" element={<Cookies />} />
-                <Route path="*" element={<NotFound />} />
+                <Route path="*" element={privatePage("Página no encontrada | Reveta", <NotFound />)} />
               </Routes>
             </BrowserRouter>
           </TooltipProvider>
