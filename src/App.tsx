fee85ddb0d6493703a141124ea7a5031d64040e0
fuadel21 <159694553+fuadel21@users.noreply.github.com>
2026-07-10 15:42:08 +0200

    Activa scroll arriba al navegar

--- src/App.tsx
+++ src/App.tsx
@@ -34,6 +34,7 @@ import Checkout from "./pages/Checkout";
 import CallRoom from "./pages/CallRoom";
 import BoostProduct from "./pages/BoostProduct";
 import MobileSellButton from "@/components/MobileSellButton";
+import ScrollToTop from "@/components/ScrollToTop";
 
 const queryClient = new QueryClient();
 
@@ -53,6 +54,7 @@ const App = () => (
             <Toaster />
             <Sonner />
             <BrowserRouter>
+              <ScrollToTop />
               <MobileSellButton />
               <Routes>
                 <Route path="/" element={<Index />} />
